6b955117451f419eb97c17e7bf0f71bfec6c5a75
Rizzar22 <116840218+Rizzar22@users.noreply.github.com>
2022-10-27 22:17:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+window.handlePanelClicked = handlePanelClicked;
+
+const topLeft = document.querySelector(".top-left-panel");
+const topRight = document.querySelector(".top-right-panel");
+const bottomLeft = document.querySelector(".bottom-left-panel");
+const bottomRight = document.querySelector(".bottom-right-panel");
+
+const getRandomPanel = () => {
+  const panels = [topLeft, topRight, bottomLeft, bottomRight];
+  return panels[parseInt(Math.random() * panels.length)];
+};
+
+const count = document.getElementById("count");
+let clickCount = 0;
+
+count.innerText = clickCount;
+const sequence = [getRandomPanel()];
+let sequenceToGuess = [...sequence];
+
+const flash = (panel) => {
+  return new Promise((resolve, reject) => {
+    panel.className += " active";
+    setTimeout(() => {
+      panel.className = panel.className.replace(" active", "");
+      setTimeout(() => {
+        resolve();
+      }, 250);
+    }, 500);
+  });
+};
+
+let canClick = false;
+
+function handlePanelClicked(panel) {
+  console.log(panel);
+  if (!canClick) return;
+  const expectedPanel = sequenceToGuess.shift();
+  if (expectedPanel === panel) {
+    if (sequenceToGuess.length === 0) {
+      // start new round
+      sequence.push(getRandomPanel());
+      sequenceToGuess = [...sequence];
+      startFlashing();
+      clickCount++;
+      count.innerText = clickCount;
+    }
+  } else {
+    // end game
+    clickCount = 0;
+    count.innerText = clickCount;
+    alert("Helaas, je hebt een fout gemaakt. Noteer je score in het formulier en begin opnieuw. Heel erg bedankt!");
+  }
+}
+
+const startFlashing = async () => {
+  canClick = false;
+  for (const panel of sequence) {
+    await flash(panel);
+  }
+  canClick = true;
+};
+
+startFlashing();
